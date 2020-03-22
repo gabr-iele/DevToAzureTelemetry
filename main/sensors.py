68e813be28ec35b0b89dd2ed78d5dc8f9b8940cc
gabr-iele <46009881+gabr-iele@users.noreply.github.com>
2020-03-22 11:52:21 +0100

    Update sensors.py

--- main/sensors.py
+++ main/sensors.py
@@ -76,9 +76,7 @@ def start_stations():
 def gen_sas_token(uri, key, expiry=3600):
     ttl = int(time.time())+expiry
     urlToSign = urllib.parse.quote(uri, safe='')
-    sign_key = "%s\n%d" % (urlToSign, int(ttl))
     h = hmac.new(base64.b64decode(key), msg = "{0}\n{1}".format(urlToSign, ttl).encode('utf-8'), digestmod='sha256')
-    signature = urllib.parse.quote(base64.b64encode(h.digest()), safe='')
     return "SharedAccessSignature sr={0}&sig={1}&se={2}".format(urlToSign, urllib.parse.quote(base64.b64encode(h.digest()),safe=''), ttl) 
 
     
