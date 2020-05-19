1049731d77f0efac35e93e65587a2623134f8605
gabr-iele <46009881+gabr-iele@users.noreply.github.com>
2020-05-19 14:19:23 +0200

    Update main.js

--- Activity Recognition/cloud_deploy/main.js
+++ Activity Recognition/cloud_deploy/main.js
@@ -8,6 +8,7 @@ var sdk = require('azure-iot-device');
 const port = '8080';
 const connString = "YOUR_DEVICE_CONNECTION_STRING";
 
+// parameters for https connection
 var options = {
     key: fs.readFileSync('certs/key.pem'),
     cert: fs.readFileSync('certs/cert.pem'),
@@ -22,6 +23,7 @@ var server = https.createServer(options, app).listen(port, function() {
     console.log("Listening on port 8080");
 });
 
+// init MQTT Azure client
 var client = sdk.Client.fromConnectionString(connString, Protocol);
 client.open(function(err) {
     if(err) console.error("Could not connect: "+err.message);
@@ -35,7 +37,7 @@ io.on('connection', function(socket) {
     });
     socket.on('data', function(data) {
         console.log(data);
-        var eventMsg = new sdk.Message(JSON.stringify(data));
+        var eventMsg = new sdk.Message(JSON.stringify(data));   // send event data to the Hub
         client.sendEvent(eventMsg, function(err) {
             if(err) console.log("Error: "+err.toString());
         });
